de33c9bedecc1d64a1d8b020107d30f73ed9c0ce
brival2 <brian.valcarcel98@gmail.com>
2020-04-21 22:28:44 -0400

    added tilemap drawing

---
+++ tileset/test_tilemap.lua
@@ -0,0 +1,60 @@
+return {
+  version = "1.2",
+  luaversion = "5.1",
+  tiledversion = "1.3.4",
+  orientation = "orthogonal",
+  renderorder = "right-down",
+  width = 10,
+  height = 10,
+  tilewidth = 16,
+  tileheight = 16,
+  nextlayerid = 2,
+  nextobjectid = 1,
+  properties = {},
+  tilesets = {
+    {
+      name = "DungeonTileset",
+      firstgid = 1,
+      filename = "DungeonTileset.tsx",
+      tilewidth = 16,
+      tileheight = 16,
+      spacing = 0,
+      margin = 0,
+      columns = 16,
+      image = "0x72_16x16DungeonTileset.v1.png",
+      imagewidth = 256,
+      imageheight = 160,
+      tileoffset = {
+        x = 0,
+        y = 0
+      },
+      grid = {
+        orientation = "orthogonal",
+        width = 16,
+        height = 16
+      },
+      properties = {},
+      terrains = {},
+      tilecount = 160,
+      tiles = {}
+    }
+  },
+  layers = {
+    {
+      type = "tilelayer",
+      id = 1,
+      name = "Tile Layer 1",
+      x = 0,
+      y = 0,
+      width = 10,
+      height = 10,
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = 0,
+      properties = {},
+      encoding = "base64",
+      data = "EQAAABEAAAARAAAAEQAAABEAAAARAAAAEQAAABEAAAARAAAAEQAAABEAAAAiAAAAIgAAACIAAAAiAAAAIgAAACIAAAAiAAAAIgAAABEAAAARAAAAIgAAACIAAAAiAAAAIgAAACIAAAAiAAAAIgAAACIAAAARAAAAEQAAACIAAAAiAAAAIgAAACIAAAAiAAAAIgAAACIAAAAiAAAAEQAAABIAAAAiAAAAIgAAACIAAAAiAAAAIgAAACIAAAAiAAAAIgAAABEAAAASAAAAIgAAACIAAAAiAAAAIgAAACIAAAAiAAAAIgAAACIAAAARAAAAEgAAACIAAAAiAAAAIgAAACIAAAAiAAAAIgAAACIAAAAiAAAAEQAAABIAAAAiAAAAIgAAACIAAAAiAAAAIgAAACIAAAAiAAAAIgAAABIAAAASAAAAIgAAACIAAAAiAAAAIgAAACIAAAAiAAAAIgAAACIAAAARAAAAEgAAABIAAAARAAAAEQAAABEAAAARAAAAEQAAABEAAAARAAAAEQAAAA=="
+    }
+  }
+}
---
+++ tileset/tiletest.lua
@@ -0,0 +1,70 @@
+return {
+  version = "1.2",
+  luaversion = "5.1",
+  tiledversion = "1.3.4",
+  orientation = "orthogonal",
+  renderorder = "right-down",
+  width = 10,
+  height = 10,
+  tilewidth = 32,
+  tileheight = 32,
+  nextlayerid = 2,
+  nextobjectid = 1,
+  properties = {},
+  tilesets = {
+    {
+      name = "test1",
+      firstgid = 1,
+      tilewidth = 20,
+      tileheight = 20,
+      spacing = 0,
+      margin = 0,
+      columns = 1,
+      image = "loveexample/example/Grass tile.png",
+      imagewidth = 20,
+      imageheight = 20,
+      tileoffset = {
+        x = 0,
+        y = 0
+      },
+      grid = {
+        orientation = "orthogonal",
+        width = 20,
+        height = 20
+      },
+      properties = {},
+      terrains = {},
+      tilecount = 1,
+      tiles = {}
+    }
+  },
+  layers = {
+    {
+      type = "tilelayer",
+      id = 1,
+      name = "Tile Layer 1",
+      x = 0,
+      y = 0,
+      width = 10,
+      height = 10,
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = 0,
+      properties = {},
+      encoding = "lua",
+      data = {
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 1, 1, 1, 0, 0, 0, 0, 0,
+        0, 0, 1, 1, 1, 0, 1, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+      }
+    }
+  }
+}
